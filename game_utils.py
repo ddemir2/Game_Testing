d380ff4db5e4d48755cdec81a9bd4bf5af6331e0
Damir Demirovic <damirdemirovic@Damirs-MacBook-Air.local>
2026-08-01 19:17:00 -0700

    implemented run_simple_route

--- game_utils.py
+++ game_utils.py
@@ -9,7 +9,7 @@ RIGHT = [0  ,  1]
 LEFT  = [0  , -1]
 UP    = [-1 ,  0]
 DOWN  = [1  ,  0]
-NONE  = [0  ,  0]
+END  = [0  ,  0]
 
 ZERO = 0
 GRID_SIZE = {"rows" : 4, "cols" : 4}
@@ -24,20 +24,27 @@ CELL_TEMPLATE = {
     } 
 
 
-
 class Machine:
     def __init__(self, title, manufacturer, loud_debug=False):
-        assert None not in (title, manufacturer), "no nulls allowed"
+        if None in (title, manufacturer): raise ValueError("no nulls allowed")
         self.title = title
         self.manufacturer = manufacturer
-        self.input_buffer = []
-        self.output_buffer = {"main" : []}
+        self.input_buffer = {"main" : [], "aux" : []}
+        self.output_buffer = {"main" : [], "aux" : []}
         self.loud_debug = loud_debug
+        self.num_inputs  = None
+        self.num_outputs = None
 
-    def ingest_data(self, data, method='replace'):
+    def get_output(self, channel) -> list:
+        if not self.output_buffer[channel] or len(self.output_buffer[channel]) == 0:
+            raise ValueError("cannot output an empty list")
+        return self.output_buffer[channel]
+
+    def ingest_data(self, data, channel='main', method='replace'):
         if not isinstance(data, list): raise ValueError("Ingest type is not list!")
+        if method != 'replace': raise ValueError("Only replace implemented")
         if method == 'replace':
-            self.input_buffer = list(data)
+            self.input_buffer[channel] = list(data)
         self.print_debug()
 
     def run(self):
@@ -45,6 +52,9 @@ class Machine:
     
     def report_materials(self):
         print("parent class report_materials!!")
+    
+    def print_logic(self):
+        return f"parent logic!"
 
     def print_debug(self, status = None):
         if status is None:
@@ -56,57 +66,77 @@ class Machine:
             print(f'Output Buffer: {self.output_buffer}')
             print(f"----------------------------\n")
 
-
-class Evaluator_1(Machine):
+class Evaluator(Machine):
     def __init__(self, title, manufacturer, loud_debug=False):
         super().__init__(title, manufacturer, loud_debug)
 
+class Evaluator_1(Evaluator):
+    def __init__(self, title, manufacturer, loud_debug=False):
+        super().__init__(title, manufacturer, loud_debug)
+        self.num_inputs  = 1
+        self.num_outputs = 1
+    
+    def print_logic(self):
+        return "All #s > 10"
+
     def run(self):
         self.print_debug()
-        if len(self.input_buffer) == 0 or self.input_buffer is None: raise ValueError("Cannot evaluate empty input buffer")
-        if all(x > 10 for x in self.input_buffer):
-            return True
+        if len(self.input_buffer["main"]) == 0 or self.input_buffer["main"] is None: raise ValueError("Cannot evaluate empty input buffer")
+        if all(x > 10 for x in self.input_buffer["main"]):
+            self.output_buffer["main"] = [1]
         else:
-            return False
-
-
+            self.output_buffer["main"] = [0]
 
 
 class Simple_Adder(Machine):
     def __init__(self, operand, title, manufacturer, loud_debug=False):
         super().__init__(title, manufacturer, loud_debug)
         self.operand = operand
+        self.num_inputs  = 1
+        self.num_outputs = 1
+        if not isinstance(self.operand, int): raise ValueError("adder operand must be integer")
+    
+    def print_logic(self):
+        return f"+ ({self.operand})"
 
     def run(self):
-        temp = np.array(self.input_buffer)
+        temp = np.array(self.input_buffer["main"])
         result = np.add(temp, self.operand)
         self.output_buffer["main"] = result.tolist()
-        return self.output_buffer["main"]
     
     def update_operand(self, new_operand):
-        if new_operand not in range(1,999): raise ValueError("Improper operand")
+        if new_operand not in range(1,999) or isinstance(new_operand, bool): raise ValueError("Improper operand")
         self.operand = new_operand
+
 
 class Input_Stream(Machine):
     def __init__(self, input_data, title, manufacturer, loud_debug=False):
         super().__init__(title, manufacturer, loud_debug)
-        assert input_data and isinstance(input_data, list), "input failure"
-        self.output_buffer = {"main": input_data}
-    
+        if not input_data or not isinstance(input_data, list): raise ValueError("input failure")
+        self.input_buffer["main"] = input_data
+        self.output_buffer["main"] = []
+        self.num_inputs  = 1
+        self.num_outputs = 1
+        
+    def print_logic(self):
+        return ""
+
     def run(self):
         self.print_debug()
-        return self.output_buffer["main"]
-
+        if not self.input_buffer["main"] or self.input_buffer["main"] == []:
+            raise ValueError("Cannot run input stream with empty input buffer")
+        self.output_buffer["main"] = self.input_buffer["main"]
 
 
 class Map:
-    def __init__(self, GRID_SIZE_LOCAL):
+    def __init__(self, GRID_SIZE_LOCAL, loud_debug=False):
         self.grid = [[copy.deepcopy(CELL_TEMPLATE) for _ in range(GRID_SIZE_LOCAL["cols"])] for _ in range(GRID_SIZE_LOCAL["rows"])]
-    
+        self.loud_debug = loud_debug
+
     def install_machine(self, obj, install_location, output_directions):
         row, col = install_location
-        assert 0 <= row < len(self.grid) and 0 <= col < len(self.grid[0]), "install_location out of bounds"
-        assert self.grid[row][col]["status"] == "empty", "install location already occupied"
+        if not (0 <= row < len(self.grid)) or not (0 <= col < len(self.grid[0])): raise ValueError("install_location out of bounds")
+        if self.grid[row][col]["status"] != "empty": raise ValueError("install location already occupied")
         if output_directions is not None and not isinstance(output_directions, list):
             raise ValueError('Directions not entered as list')
         self.grid[row][col]["obj"] = obj
@@ -116,26 +146,218 @@ class Map:
             for x in output_directions:
                 self.grid[row][col]["output_directions"].append(x)
 
-    def print_grid(self):
-        if not self.grid:
-            print("Grid is empty.")
-            return
-
-        col_count = len(self.grid[0])
-        cell_width = 5
-
-        print("   " + " ".join(f"{col:^{cell_width}}" for col in range(col_count)))
-        print("   " + "-" * (col_count * (cell_width + 1) + 2))
+    def new_print_grid(self):
+        print('\n\n')
 
         for row_idx, row in enumerate(self.grid):
             formatted_row = []
             for cell in row:
-                if cell is None:
-                    display = "."
-                elif hasattr(cell, "title"):
-                    display = cell.title
-                else:
-                    display = str(cell)
-                formatted_row.append(f"{display:^{cell_width}}")
+                display = "|------------------"
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
 
-            print(f"{row_idx:>2} |" + "|".join(formatted_row))
+            for cell in row:
+                if UP in cell["output_directions"]:
+                    display = "|        ^         "
+                else:
+                    display = "|                  "
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                else:
+                    display = '|' + cell["obj"].title.center(18)
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                else:
+                    display = '| ' + cell["obj"].print_logic().center(16) + ' '
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                elif RIGHT in cell['output_directions']:
+                    display = '|                 ' + '>'
+                elif LEFT in cell['output_directions']:
+                    display = '|<                 '
+                else:
+                    display = '|                  '
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                else:
+                    display = '|   INPUT (MAIN):  '
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                elif cell['status'] == 'occupied':
+                    list_just_numbers = ",".join(str(x) for x in cell["obj"].input_buffer["main"])
+                    display = '|' + list_just_numbers.center(18)
+                else:
+                    display = '|               '
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                else:
+                    display = '|  OUTPUT  (MAIN): '
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if cell['status'] == 'empty':
+                    display = '|                  '
+                elif cell['status'] == 'occupied':
+                    list_just_numbers = ",".join(str(x) for x in cell["obj"].output_buffer["main"])
+                    display = '|' + list_just_numbers.center(18)
+                else:
+                    display = '|                  '
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+            for cell in row:
+                if DOWN in cell["output_directions"]:
+                    display = "|        \/        "
+                else:
+                    display = "|                  "
+                formatted_row.append(f"{display}")
+            print("".join(formatted_row) + '|')
+            formatted_row = []
+
+        print('\n\n')
+
+    def search_grid(self, function=None):
+        collection = []
+        if function:
+            for index_row, row in enumerate(self.grid):
+                for index_col, col in enumerate(row):
+                    if function(self.grid[index_row][index_col]["obj"]) == True:
+                        temp = [index_row, index_col]
+                        collection.append(temp)
+                        #print(f'row {index_row}, col {index_col}')
+        return collection
+
+    def run_simple_route(self):
+        # find input stream and output directions
+        coordinates_input_stream = self.search_grid(is_input_stream)
+        if len(coordinates_input_stream) != 1: raise ValueError("improper number of input streams")
+        row_input_stream, col_input_stream = coordinates_input_stream[0]
+        obj_input_stream = self.grid[row_input_stream][col_input_stream]['obj']
+
+        # find input stream's output
+        direction_output = self.grid[row_input_stream][col_input_stream]['output_directions']
+        if len(direction_output) != 1: raise ValueError("only one output allowed")
+        row_output = row_input_stream + direction_output[0][0]
+        col_output = col_input_stream + direction_output[0][1]
+        obj_output = self.grid[row_output][col_output]['obj']
+        
+        # set up loop and run
+        obj_current = obj_input_stream
+        obj_next    = obj_output
+        while obj_current and obj_next:
+            connect_and_run(obj_current, obj_next)
+            row_current     = row_output
+            col_current     = col_output
+            obj_current     = obj_next
+            
+            if isinstance(obj_current, Evaluator):
+                obj_current.run()
+                obj_current = None
+            else:
+                direction_output = self.grid[row_current][col_current]['output_directions']
+                if len(direction_output) != 1: raise ValueError("only one output allowed")
+                row_output = row_current + direction_output[0][0]
+                col_output = col_current + direction_output[0][1]
+                obj_next = self.grid[row_output][col_output]['obj']
+
+
+
+
+#-----------HELPER FUNCTIONS-------------------------
+
+def is_evaluator(obj) -> bool:
+    if isinstance(obj, Evaluator_1):
+        return True
+    else:
+        return False
+    
+def is_input_stream(obj) -> bool:
+    if isinstance(obj, Input_Stream):
+        return True
+    else:
+        return False
+
+def is_any_machine(obj) -> bool:
+    if isinstance(obj, Machine):
+        return True
+    else:
+        return False
+
+def uninitialized_i_o(obj) -> bool:
+    if not obj:
+        return False
+    elif not obj.num_inputs or not obj.num_outputs:
+        return True
+    else:
+        return False
+
+
+def connect_and_run(machine1, machine2)->bool:
+    '''
+    Calls run() member method on machine 1, copies relevant output(s)
+    from machine 1 and passes it to machine 2's input(s) via the 
+    ingest() member method.
+
+    PARAMETERS: machine1 and machine2, which must be subclasses
+    of Machine.
+
+    RETURNS: True upon success, False upon failure
+    '''
+
+    if not isinstance(machine1, Machine) or type(machine1) == Machine: 
+        raise ValueError("improper class")
+    if not isinstance(machine2, Machine) or type(machine2) == Machine: 
+        raise ValueError("improper class")
+    if machine1.num_outputs != 1 or machine2.num_inputs != 1: 
+        raise ValueError("Only 1 output to 1 input is supported at this time")
+    if not machine1.input_buffer["main"] or machine1.input_buffer["main"] == []: 
+        raise ValueError("Machine1 Input Buffer is empty or null")
+
+    temp = None
+    success = True
+
+    try:
+        machine1.run()
+        temp = machine1.get_output('main')
+        if not temp: 
+            raise ValueError("machine1 output is null")
+        machine2.ingest_data(data=temp, channel='main', method='replace')
+    except:
+        print(f'Unspecified error: {Exception}')
+        success = False
+
+    return success
--- main.py
+++ main.py
@@ -1,119 +1,25 @@
 '''
-Version 1.0
-Tired Bison
+Version 1.1
+Gurgling Rapids
+Implemented run_simple_route
 '''
 
 import game_utils as gu
 
 if __name__ == "__main__":
-    adder_1      = gu.Simple_Adder(operand = 1,  title="Adder 1", manufacturer="DD", loud_debug=False)
-    adder_2      = gu.Simple_Adder(operand = 2,  title="Adder 2", manufacturer="DD", loud_debug=False)
-    adder_3      = gu.Simple_Adder(operand = 3, title="Adder 3", manufacturer="DD", loud_debug=False)
-    input_stream = gu.Input_Stream([1,2,3,4,5], "Input Stream", "N/A", loud_debug=False)
-    eval_1       = gu.Evaluator_1(title="eval_1", manufacturer='DD', loud_debug=False)
-    mymap        = gu.Map(GRID_SIZE_LOCAL = gu.GRID_SIZE)
+    adder_1      = gu.Simple_Adder(operand = 1,            title="ADDER_1",      manufacturer="DD",  loud_debug=False)
+    adder_2      = gu.Simple_Adder(operand = 3,            title="ADDER_2",      manufacturer="DD",  loud_debug=False)
+    adder_3      = gu.Simple_Adder(operand = 6,            title="ADDER_3",      manufacturer="DD",  loud_debug=False)
+    input_stream = gu.Input_Stream(input_data=[1,2,3,4,5], title="INPUT_STREAM", manufacturer="N/A", loud_debug=False)
+    eval_1       = gu.Evaluator_1(                         title="EVAL_1",       manufacturer='DD',  loud_debug=False)
+    mymap        = gu.Map(GRID_SIZE_LOCAL = gu.GRID_SIZE, loud_debug=True)
 
     mymap.install_machine(input_stream, [0,0], output_directions=[gu.RIGHT])
-    mymap.install_machine(adder_1, [0,1], output_directions=[gu.DOWN])
-    mymap.install_machine(adder_2, [1,1], output_directions=[gu.RIGHT])
-    mymap.install_machine(adder_3, [1,2], output_directions=[gu.RIGHT])
-    mymap.install_machine(eval_1,  [1,3], output_directions=[gu.NONE])
-    
+    mymap.install_machine(adder_1,      [0,1], output_directions=[gu.DOWN])
+    mymap.install_machine(adder_2,      [1,1], output_directions=[gu.RIGHT])
+    mymap.install_machine(adder_3,      [1,2], output_directions=[gu.RIGHT])
+    mymap.install_machine(eval_1,       [1,3], output_directions=[gu.END])
 
-    current_row = 0
-    current_col = 0
-    temp = mymap.grid[current_row][current_col]["obj"].run()
-    direction = mymap.grid[current_row][current_col]["output_directions"][0]
-    destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-    mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
 
-    current_row = 0; current_col = 1
-    temp = mymap.grid[current_row][current_col]["obj"].run()
-    direction = mymap.grid[current_row][current_col]["output_directions"][0]
-    destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-    mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-    current_row = 1; current_col = 1
-    temp = mymap.grid[current_row][current_col]["obj"].run()
-    direction = mymap.grid[current_row][current_col]["output_directions"][0]
-    destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-    mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-    current_row = 1; current_col = 2
-    temp = mymap.grid[current_row][current_col]["obj"].run()
-    direction = mymap.grid[current_row][current_col]["output_directions"][0]
-    destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-    mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-    current_row = 1; current_col = 3
-    outcome = mymap.grid[current_row][current_col]["obj"].run()
-
-    
-
-    end_game = False
-    menu_choice = 0
-    machine_change = 0
-
-    while not end_game:
-        mymap.print_grid()
-
-        current_row = 0
-        current_col = 0
-        temp = mymap.grid[current_row][current_col]["obj"].run()
-        direction = mymap.grid[current_row][current_col]["output_directions"][0]
-        destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-        mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-        current_row = 0; current_col = 1
-        temp = mymap.grid[current_row][current_col]["obj"].run()
-        direction = mymap.grid[current_row][current_col]["output_directions"][0]
-        destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-        mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-        current_row = 1; current_col = 1
-        temp = mymap.grid[current_row][current_col]["obj"].run()
-        direction = mymap.grid[current_row][current_col]["output_directions"][0]
-        destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-        mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-        current_row = 1; current_col = 2
-        temp = mymap.grid[current_row][current_col]["obj"].run()
-        direction = mymap.grid[current_row][current_col]["output_directions"][0]
-        destination_row, destination_col = [x + y for x, y in zip([current_row, current_col], direction)]
-        mymap.grid[destination_row][destination_col]["obj"].ingest_data(data=temp, method="replace")
-
-        current_row = 1; current_col = 3
-        outcome = mymap.grid[current_row][current_col]["obj"].run()
-
-        if outcome:
-            end_game = True
-            print("You passed level 1")
-        else:
-            print(f"\n-----------------------")
-            print(f"Change a machine to achieve the goal [ALL OUTPUT STREAM ITEMS MUST BE > 10]:")
-            print(f"\t1) Adder A: +1    |   Location: [0,1]")
-            print(f"\t2) Adder A: +2    |   Location: [1,1]")
-            print(f"\t3) Adder A: +3   |   Location: [1,2]")
-            # Reset choices so the user is prompted each loop iteration
-            menu_choice = 0
-            machine_change = 0
-            while menu_choice not in [1,2,3]:
-                try:
-                    print(f"Type your choice here: ")
-                    menu_choice = int(input())
-                except ValueError:
-                    menu_choice = 0
-            while machine_change not in range(1,999):
-                try:
-                    print(f"To which value would you like to change the adder? Type your answer here:")
-                    machine_change = int(input())
-                except ValueError:
-                    machine_change = 0
-            if menu_choice == 1:
-                mymap.grid[0][1]["obj"].update_operand(machine_change) 
-            elif menu_choice == 2:
-                mymap.grid[1][1]["obj"].update_operand(machine_change)
-            elif menu_choice == 3:
-                mymap.grid[1][2]["obj"].update_operand(machine_change)
-            else:
-                raise ValueError("Unknown Game State")
+    mymap.run_simple_route()
+    mymap.new_print_grid()
